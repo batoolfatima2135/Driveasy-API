9dd51abaeca8f2116bdbc8191cd9c2d0e0d13370
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-26 14:26:42 +0500

    add create method

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
