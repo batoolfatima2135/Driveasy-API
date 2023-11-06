95ffbb8f60f3cdad277c60c7fb627ff507995cf6
othniel2471 <othnielomaidu@gmail.com>
2023-11-07 00:08:58 +0100

    removed unused code from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :reservations, only: [:index, :show,:create, :destroy]
   resources :cars, only: [:index, :create, :show, :destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :users, only: [:create]
 end
