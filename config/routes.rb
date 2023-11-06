251fe2c7e420743cbbe71f23fc6365fa267f6d57
othniel2471 <othnielomaidu@gmail.com>
2023-11-07 00:08:11 +0100

    updated routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post 'users', to: 'users#create'
+  # post 'users', to: 'users#create'
+  resources :users, only: [:create]
 end
